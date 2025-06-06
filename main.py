b3fbf76b0292b545fc5ebbd50c4465a1db62b310
Test <kilianberteaudca@gmail.com>
2025-06-05 22:36:06 -0400

    Ajout main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+"""--------Projet de session - Simulation de l'atterrissage d'un avion----------
+
+    - Kilian Berteaud
+    - Alexis Chenuet
+    - Pierrick Loranchet
+
+Cours MGA802, Session Été 2025
+"""
+
+#PROGRAMME PRINCIPAL
