a7ffb47d91188fc767586d11530390718400be1d
Test <kilianberteaudca@gmail.com>
2025-06-11 11:24:07 -0400

    Simplification des fonctions et utilisation de la classe Piste et de la future classe Meteo

--- Class.py
+++ Class.py
@@ -1,68 +1,106 @@
-from math import sqrt, cos, sin, tan, radians, log
+from math import sqrt, cos, sin, tan, radians
 
 
 class Avion:
-    angle_de_descente = -3
+    angle_de_descente = -3 #Degrés
     g = 32.2 #ft/s²
     temps_roue_libre = 3 #Secondes
 
     def __init__(self,code, poids_atterrissage, surface_alaire,
-                 coefficient_portance_max_atterrissage,):
+                 coefficient_portance_max_atterrissage,meteo,piste):
         self.code = code
         self.W_LA = poids_atterrissage
         self.S = surface_alaire
         self.Cl_max_LA = coefficient_portance_max_atterrissage
         self.charge_alaire = self.W_LA/self.S
+        self.piste = piste
+        self.mu = self.piste.ajout_coefficient_friction()
+        self.meteo = meteo
+        self.density = self.meteo.density()
+        # Poussée au sol pour l'instant nulle
+        self.T_sol = 0
 
     def calcul_V_stall(self):
-        self.V_stall = sqrt(self.W_LA / (0.5 * self.density * self.S * self.Cl_max_LA))
-        return self.V_stall
+        return sqrt(self.W_LA / (0.5 * self.density * self.S * self.Cl_max_LA))
+
+    def calcul_V_TD(self):
+        V_stall = self.calcul_V_stall()
+        return 1.15*V_stall
 
     def calcul_S_FR(self):
-        self.V_TD = 1.15*self.V_stall
-        self.S_FR = self.temps_roue_libre * self.V_TD
-        return self.S_FR
+        V_TD = self.calcul_V_TD()
+        return self.temps_roue_libre * V_TD
 
     def calcul_f1(self):
-        self.f1 = (self.T_sol/self.mu)+self.mu
-        return self.f1
+        return (self.T_sol/self.mu)+self.mu
 
 class Commercial(Avion):
-    coef_transition = 1, 23
-    n = 1.35 #Facteur de charge
-    H_OBS = 35 #Hauteur d'obstacle
 
-    def __init__(self, poids_atterrissage, surface_alaire,
-                 coefficient_portance_max_atterrissage):
-        super().__init__(poids_atterrissage, surface_alaire,
-                         coefficient_portance_max_atterrissage)
+    def __init__(self,code, poids_atterrissage, surface_alaire,
+                 coefficient_portance_max_atterrissage,meteo,piste,
+                 facteur_de_charge = 1.35,hauteur_obstacle = 35,
+                 coef_transition = 1.23):
+        super().__init__(code, poids_atterrissage, surface_alaire,
+                 coefficient_portance_max_atterrissage,meteo,piste)
+        self.n = facteur_de_charge
+        self.H_OBS = hauteur_obstacle
+        self.coef_TR = coef_transition
 
     def calcul_V_TR(self):
-        self.V_TR = self.coef_transition * self.V_stall
-        return self.V_TR
+        V_stall = self.calcul_V_stall()
+        return self.coef_TR * V_stall
 
     def calcul_R(self):
-        self.R = (self.V_TR**2)/(self.g*(self.n-1))
-        return self.R
+        V_TR = self.calcul_V_TR()
+        return (V_TR**2)/(self.g*(self.n-1))
 
     def calcul_H_TR(self):
-        self.H_TR = self.R*(1-cos(radians(self.angle_de_descente)))
-        return self.H_TR
+        R = self.calcul_R()
+        return R*(1-cos(radians(self.angle_de_descente)))
 
     def calcul_S_A(self):
-        self.S_A = (self.H_TR-self.H_OBS)/tan(radians(self.angle_de_descente))
-        return self.S_A
+        H_TR = self.calcul_H_TR()
+        return (H_TR-self.H_OBS)/tan(radians(self.angle_de_descente))
 
     def calcul_S_TR(self):
-        self.S_TR = -self.R*sin(radians(self.angle_de_descente))
-        return self.S_TR
+        R = self.calcul_R()
+        return -R*sin(radians(self.angle_de_descente))
 
 
 
 class Militaire(Avion):
-    coef_transition = 1.15
-    n = 2 #Facteur de charge
-    H_OBS = 50 #Hauteur d'obstacle
+
+    def __init__(self, code, poids_atterrissage, surface_alaire,
+                 coefficient_portance_max_atterrissage, meteo, piste,
+                 facteur_de_charge = 2,hauteur_obstacle = 50,
+                 coef_transition = 1.15):
+        super().__init__(code, poids_atterrissage, surface_alaire,
+                         coefficient_portance_max_atterrissage, meteo, piste)
+        self.n = facteur_de_charge
+        self.H_OBS = hauteur_obstacle
+        self.coef_TR = coef_transition
+
+    def calcul_V_TR(self):
+        V_stall = self.calcul_V_stall()
+        return self.coef_TR * V_stall
+
+    def calcul_R(self):
+        V_TR = self.calcul_V_TR()
+        return (V_TR ** 2) / (self.g * (self.n - 1))
+
+    def calcul_H_TR(self):
+        R = self.calcul_R()
+        return R * (1 - cos(radians(self.angle_de_descente)))
+
+    def calcul_S_A(self):
+        H_TR = self.calcul_H_TR()
+        return (H_TR - self.H_OBS) / tan(radians(self.angle_de_descente))
+
+    def calcul_S_TR(self):
+        R = self.calcul_R()
+        return -R * sin(radians(self.angle_de_descente))
+
+
 # Class aéroport
 
 class Aeroport:
@@ -101,24 +139,6 @@ class Piste(Aeroport):
         # Appel du constructeur parent
         super().__init__(nom, code, ville, coordonnees, altitude)
 
-    def calcul_V_TR(self):
-        return self.coef_transition * self.V_stall
-
-    def calcul_R(self):
-        self.R = (self.V_TR ** 2) / (self.g * (self.n - 1))
-        return self.R
-
-    def calcul_H_TR(self):
-        self.H_TR = self.R*(1-cos(radians(self.angle_de_descente)))
-        return self.H_TR
-
-    def calcul_S_A(self):
-        self.S_A = (self.H_TR-self.H_OBS)/tan(radians(self.angle_de_descente))
-        return self.S_A
-
-    def calcul_S_TR(self):
-        self.S_TR = -self.R*sin(radians(self.angle_de_descente))
-        return self.S_TR
         self.numero_piste = numero_piste
         self.surface = surface.lower()  # Normalisation en minuscules
         self.largeur = largeur  # en mètres
