72d2590020b93a7b49b72e6b7ef5fa7988de70c8
PierrickLoranchet <pierrickloranchet@gmail.com>
2025-06-11 20:24:19 -0400

    Amélioration des méthodes pistes pour compatibilité avec la sous-classe. Ajout des méthodes longueur et largeur dans la sous classe piste

--- classes/aeroports/aeroport.py
+++ classes/aeroports/aeroport.py
@@ -38,8 +38,18 @@ class Aeroport:
     def pistes(self,runways_df):
         filtre_code= runways_df[runways_df["ident"] == self.code]
         if not filtre_code.empty:
-            pistes = [f"{row['le_ident']}-{row['he_ident']}" for index, row in filtre_code.iterrows()]
-            return pistes
+            pistes_le = [f"{row['le_ident']}" for index, row in filtre_code.iterrows()]
+            pistes_he = [f"{row['he_ident']}" for index, row in filtre_code.iterrows()]
+            return pistes_le, pistes_he
+        else:
+            print("Aeroport non trouvé")
+
+    #Permet de concaténer le_ident et he_ident pour afficher les pistes de chaque aéroport
+    def afficher_pistes(self,runways_df):
+        filtre_code= runways_df[runways_df["ident"] == self.code]
+        if not filtre_code.empty:
+            pistes_affichage = [f"{row['le_ident']}-{row['he_ident']}" for index, row in filtre_code.iterrows()]
+            return pistes_affichage
         else:
             print("Aeroport non trouvé")
 
@@ -47,4 +57,4 @@ class Aeroport:
         print(f"Aéroport: {self.nom(runways_df)} ({self.code})")
         print(f"Coordonnées: {self.latitude(runways_df)}, {self.longitude(runways_df)}")
         print(f"Altitude: {self.altitude(runways_df)} ft")
-
+        print(f"Les pistes de cette aéroport sont: {self.afficher_pistes(runways_df)}")
