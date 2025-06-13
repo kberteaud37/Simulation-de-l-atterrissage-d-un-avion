0ad9ca98b4aa46fb5a1300ccfcb662abd6c64d35
Test <kilianberteaudca@gmail.com>
2025-06-13 16:01:23 -0400

    Finitions

--- Extra_function.py
+++ Extra_function.py
@@ -22,7 +22,7 @@ def get_float_input(prompt):
 # Fonction qui compare la longueur des pistes avec la distance nécessaire à l'atterrissage
 def compare(avion, piste, coef_secu=1.67):
     distance_necessaire = (avion.calcul_S_B()+avion.calcul_S_FR()+avion.calcul_S_TR()) * coef_secu
-    print(f"La distance nécessaire est : {distance_necessaire} ft")
+    print(f"La distance nécessaire est : {distance_necessaire : .2f} ft")
     print(f"La longueur de la piste est : {piste.longueur()} ft")
 
     df_runways = recuperer_runways()
@@ -32,13 +32,12 @@ def compare(avion, piste, coef_secu=1.67):
 
     max_recherches = 20
     nb_recherches = 0
+    print(f"Test pour l'aéroport {piste.nom()}...")
 
     while distance_necessaire >= piste.longueur():
         if nb_recherches >= max_recherches:
             raise RuntimeError("Trop de recherches effectuées, aucun aéroport viable trouvé.")
         nb_recherches += 1
-        print("Atterrissage non sûr : la distance nécessaire dépasse la longueur de la piste.\n"
-              "Recherche d'une piste d'atterrissage sûre en cours...")
 
         for num_piste in pistes["runway_ident"]:
             piste_test = classes.aeroports.Piste(code_aeroport_courant, df_runways, num_piste)
@@ -46,6 +45,8 @@ def compare(avion, piste, coef_secu=1.67):
                 piste = piste_test
                 break
         else:
+            print("Atterrissage non sûr : la distance nécessaire dépasse la longueur de la piste.\n"
+                  "Recherche d'une piste d'atterrissage sûre en cours...")
             aeroport_proche = trouver_aeroport_proche(aeroport)
             code_aeroport_courant = aeroport_proche["ident"]
             pistes = df_runways[df_runways["ident"] == code_aeroport_courant]
@@ -53,11 +54,12 @@ def compare(avion, piste, coef_secu=1.67):
             ligne = pistes.iloc[0]
             nouvelle_piste = classes.aeroports.Piste(ligne["ident"], df_runways, ligne["runway_ident"])
             aeroport.append(nouvelle_piste)
+            print(f"Test pour l'aéroport {nouvelle_piste.nom()}...")
             continue  # relance le while
         break  # sortie si piste compatible trouvée
 
     print(f"Vous pouvez atterrir à {piste.nom()} sur la piste {piste.n_piste}")
-    print(f"La longueur de la piste est {piste.longueur()} ft et la distance nécessaire de {distance_necessaire} ft")
+    print(f"La longueur de la piste est {piste.longueur()} ft et la distance nécessaire de {distance_necessaire:.2f} ft")
 
 
 def trouver_aeroport_proche(exclusions):
@@ -84,7 +86,6 @@ def trouver_aeroport_proche(exclusions):
     dist, idx = tree.query(coord_depart)
     plus_proche = df_filtre.iloc[idx]
 
-    print(f"Aéroport le plus proche (hors exclusions) : {plus_proche['ident']} à {dist * 6371:.2f} km")
     return plus_proche
 
 
